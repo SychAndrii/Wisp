71f1ec64d92d45ebb97a31c29ddfd30c97ab1650
SychAndrii <asych@myseneca.ca>
2024-11-11 01:30:32 -0500

    Set up connection to Discord API with bot token

--- Discord/Program.cs
+++ Discord/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace Discord
+﻿using Discord.WebSocket;
+
+namespace Discord
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DiscordSocketClient client = SetupDiscordClient();
+            await LoginDiscordClient(client);
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private static DiscordSocketClient SetupDiscordClient()
+        {
+            var config = new DiscordSocketConfig { MessageCacheSize = 100 };
+            DiscordSocketClient client = new();
+            client.Ready += DiscordClientReady;
+            return client;
+        }
+
+        private static async Task LoginDiscordClient(DiscordSocketClient client)
+        {
+            var token = Environment.GetEnvironmentVariable("DiscordToken");
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+        }
+
+        private static Task DiscordClientReady()
+        {
+            Console.WriteLine("Discord client ready!");
+            return Task.CompletedTask;
         }
     }
 }
