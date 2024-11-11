d14448a36a2ebfb2388a1c1cdb23e4588f67df36
SychAndrii <asych@myseneca.ca>
2024-11-11 12:16:57 -0500

    Set up echo command for a specific guild.

---
+++ Discord/App.cs
@@ -0,0 +1,50 @@
+﻿using Discord.Interactions;
+using Discord.WebSocket;
+
+namespace Discord
+{
+    internal class App(DiscordSocketClient client,
+                       InteractionService interactionService,
+                       IServiceProvider serviceProvider)
+    {
+        private readonly DiscordSocketClient client = client;
+        private readonly InteractionService interactionService = interactionService;
+        private readonly IServiceProvider serviceProvider = serviceProvider;
+
+        public async Task Startup(string token, ulong? guildID = null)
+        {
+            client.Ready += () => OnClientReady(guildID);
+            client.InteractionCreated += OnClientInteractionCreated;
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+        }
+
+        private async Task OnClientInteractionCreated(SocketInteraction interaction)
+        {
+            var context = new SocketInteractionContext(client, interaction);
+            var result = await interactionService.ExecuteCommandAsync(context, serviceProvider);
+
+            if (!result.IsSuccess)
+            {
+                Console.WriteLine($"Error: {result.ErrorReason}");
+                if (interaction.HasResponded == false)
+                    await interaction.RespondAsync("An error occurred while executing the command.");
+            }
+        }
+
+        private async Task OnClientReady(ulong? guildID)
+        {
+            Console.WriteLine("Bot is connected!");
+            await interactionService.AddModulesAsync(typeof(App).Assembly, serviceProvider);
+
+            if (guildID.HasValue)
+            {
+                await interactionService.RegisterCommandsToGuildAsync(guildID.Value);
+            }
+            else
+            {
+                await interactionService.RegisterCommandsGloballyAsync();
+            }
+        }
+    }
+}
--- Discord/Program.cs
+++ Discord/Program.cs
@@ -1,37 +1,56 @@
-﻿using Discord.WebSocket;
+﻿using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Discord
 {
     internal class Program
     {
-        static async Task Main(string[] args)
+        static ServiceProvider ConfigureServices()
         {
-            DiscordSocketClient client = SetupDiscordClient();
-            await LoginDiscordClient(client);
+            var collection = new ServiceCollection();
+
+            collection
+                .AddSingleton(provider =>
+                {
+                    var client = new DiscordSocketClient(new DiscordSocketConfig { MessageCacheSize = 100 });
+                    return client;
+                })
+                .AddSingleton(provider =>
+                {
+                    var client = provider.GetRequiredService<DiscordSocketClient>();
+                    var config = new InteractionServiceConfig
+                    {
+                        UseCompiledLambda = true,      // Drastically improves performance of command execution.
+                        DefaultRunMode = RunMode.Async // Set to Async to run commands on a separate thread
+                    };
+                    return new InteractionService(client, config);
+                })
+                .AddSingleton<App>();
+
+            return collection.BuildServiceProvider();
+        }
+
+        static async Task Main()
+        {
+            var services = ConfigureServices();
+            var app = services.GetRequiredService<App>();
+
+            string envToken = Environment.GetEnvironmentVariable("DiscordToken")
+                ?? throw new Exception("DiscordToken environment variable must be set!");
+
+            string? envGuildID = Environment.GetEnvironmentVariable("DiscordGuild");
+            if (envGuildID is not null && ulong.TryParse(envGuildID, out ulong guildID))
+            {
+                await app.Startup(envToken, guildID);
+            }
+            else
+            {
+                await app.Startup(envToken);
+            }
 
             // Block this task until the program is closed.
             await Task.Delay(-1);
-        }
-
-        private static DiscordSocketClient SetupDiscordClient()
-        {
-            var config = new DiscordSocketConfig { MessageCacheSize = 100 };
-            DiscordSocketClient client = new();
-            client.Ready += DiscordClientReady;
-            return client;
-        }
-
-        private static async Task LoginDiscordClient(DiscordSocketClient client)
-        {
-            var token = Environment.GetEnvironmentVariable("DiscordToken");
-            await client.LoginAsync(TokenType.Bot, token);
-            await client.StartAsync();
-        }
-
-        private static Task DiscordClientReady()
-        {
-            Console.WriteLine("Discord client ready!");
-            return Task.CompletedTask;
         }
     }
 }
