c5532010e5e385e348da4e433e378e4f6d237072
SychAndrii <asych@myseneca.ca>
2024-11-13 02:09:55 -0500

    Finished initial Exercise aggregate

--- ExercisesDomain/Exercise.cs
+++ ExercisesDomain/Exercise.cs
@@ -7,17 +7,22 @@ namespace ExercisesDomain
         public Guid Id { get; }
         public ExerciseName Name { get; }
         public ExerciseDescription? Description { get; }
+        public DateTime CreatedAt { get; }
 
-        private Exercise(Guid id, ExerciseName name, ExerciseDescription? desc)
+        private Exercise(Guid id, ExerciseName name, ExerciseDescription? desc, DateTime createdAt)
         {
             Id = id;
             Name = name;
             Description = desc;
+            CreatedAt = createdAt;
         }
 
-        public static Exercise Create(Guid id, ExerciseName name, ExerciseDescription? desc)
+        public static Exercise Create(Guid id,
+                                      ExerciseName name,
+                                      ExerciseDescription? desc,
+                                      DateTime createdAt)
         {
-            return new Exercise(id, name, desc);
+            return new Exercise(id, name, desc, createdAt);
         }
     }
 }
